65205a806ac025196d9a4db7c2a734e8ac1adab6
Иван <ivanandrosovv@gmail.com>
2023-07-13 16:12:18 +0300

    Something like actual start of dev??

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import TestButton from "./components/TestButton";
+import Header from "./components/Header";
+
+import './css/app.css'
 
 function App() {
   return (
     <div className="App">
       Hello world!
+        <Header/>
         <TestButton/>
     </div>
   );
---
+++ src/components/Header.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './../css/header.css'
+
+const Header = () => {
+    return (
+        <div className={"header"}>
+            <div className={"trainingTrackText"}>
+                TrainingTrack
+            </div>
+        </div>
+    );
+};
+
+export default Header;
