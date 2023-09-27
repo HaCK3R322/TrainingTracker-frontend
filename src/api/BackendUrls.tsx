31d691d76ab846798087dbc709ba95834213ea5e
dictator_zx <ivanandrosovv@gmail.com>
2023-09-27 16:00:18 +0300

    End

--- src/api/BackendUrls.tsx
+++ src/api/BackendUrls.tsx
@@ -1,6 +1,8 @@
-// let host: string = "http://localhost:8080"
-// let host: string = "http://ivanandrosovv.ru:8080"
-let host: string = "http://193.218.142.195:8080"
+let local: string = "http://localhost:8080"
+let cloud: string = "http://ivanandrosovv.ru:8080"
+// let myip: string = "http://193.218.142.195:8080"
+
+const host = cloud;
 
 let urls = {
     register: host + "/register",
